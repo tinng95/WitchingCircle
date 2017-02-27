663ae9341f63b302d310f2188de9b0afe91c313b
Tin Nguyen <tinng95@yahoo.com>
2017-02-27 10:56:03 -0600

    Try to do some draw animation
    And FAIL... miserably

--- ProjectWitch/Witch/Assets/Script/DrawCard.cs
+++ ProjectWitch/Witch/Assets/Script/DrawCard.cs
@@ -20,11 +20,32 @@ public class DrawCard : MonoBehaviour {
 
     public void getCard()
     {
-        while (Hand.childCount < 5)
+        
+        for (int i = Hand.childCount; i < 5; i++)
+        {
+            type = (CardType)Random.Range(0, (int)CardType.CARD4 + 1);
+            CardClone = Instantiate(Cards[(int)type], transform.position, Quaternion.identity) as GameObject;
+            //CardClone.SetActive(false);
+            CardClone.transform.SetParent(Hand);
+        }
+        
+        //Debug.Log("SET CARD 0 INACTIVE");
+        //Hand.transform.GetChild(0).GetComponent<GameObject>().SetActive(false);
+        //Debug.Log("BEFORE DOSOMETHING");
+       // StartCoroutine(DoSomething(0.5f));
+        //Debug.Log("AFTER DOSOMETHING");
+    }
+    IEnumerator DoSomething(float seconds)
+    {
+        //Debug.Log("TOTAL HAND CARD IS " + Hand.childCount);
+        for (int i = Hand.childCount; i < 5; i++)
         {
             type = (CardType)Random.Range(0, (int)CardType.CARD4 + 1);
             CardClone = Instantiate(Cards[(int)type], transform.position, Quaternion.identity) as GameObject;
             CardClone.transform.SetParent(Hand);
+            Debug.Log("The card is: " + i);
+            yield return new WaitForSeconds(seconds);
         }
-    }	
+       // Debug.Log("SHOULD ONLY OUTPUT 1!");
+    }
 }
